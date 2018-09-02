948eda652062043d682903016a1f072b1637460a
shawn0615 <41461825+shawn0615@users.noreply.github.com>
2018-09-01 21:33:14 -0500

    Add files via upload

--- plot2.R
+++ plot2.R
@@ -3,6 +3,6 @@ data <- read.table(householddata, header=TRUE, sep=";", stringsAsFactors=FALSE, 
 FebData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
 GAP <- as.numeric(subSetData$Global_active_power)
-png("plot1.png", width=480, height=480)
-plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+png("plot2.png", width=480, height=480)
+plot(datetime, GAP, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
