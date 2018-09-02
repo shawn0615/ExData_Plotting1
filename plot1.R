280a4857f385f92560a6915f29b0c52c50c81561
shawn0615 <41461825+shawn0615@users.noreply.github.com>
2018-09-01 21:19:56 -0500

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,7 @@
+householddata <- "household_power_consumption.txt"
+data <- read.table(householddata, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+FebData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+GAP <- as.numeric(subSetData$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(GAP, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
